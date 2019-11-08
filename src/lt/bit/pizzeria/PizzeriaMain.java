2b0efcd19315170ef2c946da1e9c3132b485dc8f
GiedriusVir <virsilasg@gmail.com>
2019-11-08 12:36:28 +0200

    Food class fields + constructor + getter #2

--- src/lt/bit/pizzeria/PizzeriaMain.java
+++ src/lt/bit/pizzeria/PizzeriaMain.java
@@ -4,6 +4,44 @@ package lt.bit.pizzeria;
 PizzeriaMain - veiksmo vieta
  */
 
-public class PizzeriaMain {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class PizzeriaMain {
+
+    public static void main(String[] args) {
+
+        List<Food> food = new ArrayList<>();
+        food.add(new Pizza("Aitriujų paprikų", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, pievagrybiai, saliamis, aitriosios paprikos, augalinis aliejus su česnaku", 32, 5.89));
+        food.add(new Pizza("Akapulko", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, pievagrybiai, virtas jautienos kumpis, Chalapos paprikos, aštrus padažas, augalinis aliejus su česnaku", 32, 5.89));
+        food.add(new Pizza("Aštrioji", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, forminis kiaulienos kumpis, saliamis, Chalapos paprikos, džiovintos aštriosios paprikos, aštrus padažas, augalinis aliejus su česnaku", 32, 5.89));
+        food.add(new Pizza("Chorizo", "Kvietinių miltų pagrindas, pomidorų padažas fermentinis sūris, Chorizo dešra, aitrosios paprikos, augalinis aliejus su česnaku", 32, 5.99));
+        food.add(new Wrap("Suktinukai su kumpiu", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, forminis kiaulienos kumpis, pievagrybiai, augalinis aliejus su česnaku", 4.89));
+        food.add(new Wrap("Suktinukai su malta kiauliena", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, malta kiauliena, švž. paprikos, Chalapos paprikos, augalinis aliejus su česnaku", 4.89));
+        food.add(new Wrap("Suktinukai su vištiena", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, vištiena, pievagrybiai, konservuotos paprikos, augalinis aliejus su česnaku", 4.89));
+        food.add(new Wrap("Suktinukai vegetariški", "Kvietinių miltų pagrindas, pomidorų padažas, fermentinis sūris, pievagrybiai, konservuotos paprikos, artišokai, Salsa padažas, augalinis aliejus su česnaku", 4.89));
+        food.add(new Tortilla("Tortilija su sojomis", "Kviečių tortilija įdaryta keptomis sojos lazdelėmis, graikiško tipo sūrio kubeliais, svogūnų griežinėliais, pomidorais ir traškiomis ledkalnio salotomis", 2.95));
+        food.add(new Tortilla("Tortilija su vištiena", "Tortilijos su vištiena paplotėlis įdarytas juostelėmis supjaustyta vištienos file, graikiško tipo sūrio kubeliais, svogūnų griežinėliais, pomidorais, ledkalnio salotomis ir Chalapos aitriąja paprika, suteikiančia reikiamo aštrumo", 2.95));
+
+        Iterator<Food> pizzaIterator = food.iterator();
+        Iterator<Food> wrapIterator = food.iterator();
+        Iterator<Food> tortillaIterator = food.iterator();
+        System.out.println("PICOS");
+        printFoodItems(pizzaIterator);
+        System.out.println("SUKTINUKAI");
+        printFoodItems(wrapIterator);
+        System.out.println("TORTILIJOS");
+        printFoodItems(tortillaIterator);
+    }
+
+    private static void printFoodItems(Iterator<Food> iterator) {
+        while (iterator.hasNext()) {
+            Food foodItem = iterator.next();
+            if (foodItem instanceof Pizza) {
+                System.out.println(foodItem);
+            }
+        }
+    }
 
 }
---
+++ src/lt/bit/pizzeria/Wrap.java
@@ -0,0 +1,35 @@
+package lt.bit.pizzeria;
+
+class Wrap implements Food {
+
+    private String name;
+    private String ingredients;
+    private double price;
+
+    Wrap(String name, String ingredients, double price) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n Sudėtis: " +
+                ingredients + "\n Kaina: " + price + "€";
+    }
+}
